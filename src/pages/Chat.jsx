e13d19921b7e97bb9d7795320bdab2d0f9c06bef
CaioLira18 <caiozao1212@gmail.com>
2026-02-05 11:12:27 -0300

    [feat] - Update ChatPage

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,33 +1,44 @@
 import React from 'react'
 
 export const Chat = () => {
-
     const rightPosition = true;
     const loading = true;
 
     return (
         <div>
             <div className="containerChat">
-                <div className="containerBox">
+                {/* Mensagem do Usuário - Alinhada à Esquerda */}
+                <div className="containerBox userAlign">
                     <div className="messageBox">
                         <div className="userProfile">
-                            <i class="fa-solid fa-user"></i>
+                            <i className="fa-solid fa-user"></i>
                         </div>
                         <div className="messageContent">
-                            <div className="messageText">Hello, how can I help you today?</div>
+                            <div className="messageText">Bom dia, Gostaria de realizar uma transação de 10 reais para pedro</div>
                             <div className="messageTime">10:30 AM</div>
                         </div>
                     </div>
                 </div>
+
+                {/* Mensagens do Bot - Alinhadas à Direita */}
                 {rightPosition && (
-                    <div className="containerBox">
+                    <div className="containerBox botAlign">
                         <div className="messageBox">
                             <div className="messageContent">
-                                <div className="messageText">Hello, how can I help you today?</div>
+                                <div className="messageText">Pix de 10 reais para pedro</div>
                                 <div className="messageTime">10:30 AM</div>
                             </div>
                             <div className="userProfile">
-                                <i class="fa-solid fa-robot"></i>
+                                <i className="fa-solid fa-robot"></i>
+                            </div>
+                        </div>
+                        <div className="messageBox">
+                            <div className="messageContent">
+                                <div className="messageText">Pix de 10 reais para pedro</div>
+                                <div className="messageTime">10:30 AM</div>
+                            </div>
+                            <div className="userProfile">
+                                <i className="fa-solid fa-robot"></i>
                             </div>
                         </div>
                         <div className="messageBox">
@@ -36,31 +47,35 @@ export const Chat = () => {
                                 <div className="messageTime">Sim / Não</div>
                             </div>
                             <div className="userProfile">
-                                <i class="fa-solid fa-robot"></i>
+                                <i className="fa-solid fa-robot"></i>
                             </div>
                         </div>
                         {loading && (
-                            <div className="userProfile loading">
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                            <div className="loadingWrapper">
+                                <div className="userProfile loading">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </div>
                         )}
                     </div>
                 )}
             </div>
 
-
             <div className="inputBarContainer">
                 <div className="inputBarBox">
                     <input placeholder='Digite Algo' type="text" />
-                    <div className="microphoneButton">
-                        <i class="fa-solid fa-microphone"></i>
+                    <div className='waveformIcon'>
+                        <img src="src/assets/waveform.svg" alt="" />
                     </div>
                 </div>
+                <div className="microphoneButton">
+                    <i className="fa-solid fa-microphone"></i>
+                </div>
             </div>
-        </div>
 
+        </div>
     )
 }
 
